9800387bcf6c875b9984104b1376ba746cebc2c6
shahdhamdi <117816917+shahdhamdi@users.noreply.github.com>
2024-10-06 17:14:26 +0000

    Add changes

---
+++ SQL Scripts/data_exeploring.sql
@@ -0,0 +1,11 @@
+select  * from Olympic_Games_Medal_Tally;
+
+select  * from Olympic_Athlete_Event_Results;
+
+select  * from Olympic_Athlete_Bio;
+
+select  * from Olympic_Results;
+
+select  * from Olympics_Country;
+
+select  * from Olympics_Games;
